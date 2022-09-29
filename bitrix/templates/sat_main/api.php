4bf5b7608daa271011d25c6ed5e592d624a97118
Oleksii Tkachenko <a0672188171@gmail.com>
2022-09-29 12:37:17 +0300

    wqdqwd

--- bitrix/templates/sat_main/api.php
+++ bitrix/templates/sat_main/api.php
@@ -3,7 +3,7 @@ $out = 'ok';
 $arr = array();
 $url = '';
 
-//$api_host_2 = 'http://urm.sat.ua/openws/hs/api/v1.0/'; //Продf
+//$api_host_2 = 'http://urm.sat.ua/openws/hs/api/v1.0/'; //Прод
 
 //$apiHostV2 = 'https://api.sat.ua/openws/hs/api/v2.0/'; //Прод
 
